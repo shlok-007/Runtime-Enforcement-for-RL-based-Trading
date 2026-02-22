292611a2b7e01a9cbcd550840a47180edaa16072
shlok-007 <shlok845@gmail.com>
2026-02-22 18:02:38 +0530

    Added erte policy specification for OTR, MQL and Wash Trading

---
+++ policies/OrderToTradeRatio/OTR_stub.c
@@ -0,0 +1,5 @@
+#include "F_OTR.h"
+
+// dummy function
+void OTR_run(inputs_OTR_t* inputs, outputs_OTR_t* outputs) {
+}
---
+++ policies/WashTrading/Wash_Trade_stub.c
@@ -0,0 +1,5 @@
+#include "F_WashTrading.h"
+
+// dummy function
+void WashTrading_run(inputs_WashTrading_t* inputs, outputs_WashTrading_t* outputs){
+}
